914663f9cbb911015e3e269d908a515395796c80
Megan Phillips <megphillips21@gmail.com>
2022-07-14 11:25:58 -0700

    created local settings file, moved secret key, edited database settings

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@6$^b*&)#b8i8+y^6x#4irk2jnicggkr(3@m+)770br**q_(a*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
