b97aac74ef5c356ca3dcc4985feff66487d758a6
Megan Phillips <megphillips21@gmail.com>
2022-07-17 16:32:53 -0700

    removed apps that i made wrong

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -112,3 +112,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from heroes_villians_project.local_settings import *
+except ImportError:
+    pass
