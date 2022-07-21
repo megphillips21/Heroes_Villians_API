92a8b88ead4e1806f2ab2159af9f641be3251b9d
Megan Phillips <megphillips21@gmail.com>
2022-07-20 21:08:36 -0700

    added model, admin user for super_type, updated serializer for supers.

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,4 +4,5 @@ from .models import Super
 class SupersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase']
+        fields = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase', 'super_type']
+        depth = 1
